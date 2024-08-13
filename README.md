3ab2bfd557a0bd000288520d3f1fefd18437b3d6
ycheng <ycheng22@hotmail.com>
2024-08-13 14:26:30 -0400

    everything looks good

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # stackblitz-ng-snippets
 
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ycheng22/stackblitz-ng-snippets)
+
+
+#TODO list:
+- Toggle
+- textarea
+- accordian
+- button styles
+- calculte in input box
---
+++ src/input-cal/input-cal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-cal',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-cal.component.html',
+  styleUrl: './input-cal.component.css'
+})
+export class InputCalComponent {
+
+}
