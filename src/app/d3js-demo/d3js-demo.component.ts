83299758c227178898227dfd37e4f17b15a0f3d3
ycheng <ycheng22@hotmail.com>
2024-08-13 17:10:26 -0400

    styles

---
+++ src/app/d3js-demo/d3js-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d3js-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './d3js-demo.component.html',
+  styleUrl: './d3js-demo.component.css'
+})
+export class D3jsDemoComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { MatDividerModule } from '@angular/material/divider';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MultiSelectComponent } from './app/multi-select/multi-select.component';
 import { NgSelectDemoComponent } from './app/ng-select-demo/ng-select-demo.component';
 import { InputCalComponent } from './app/input-cal/input-cal.component';
@@ -15,6 +16,7 @@ import { AgGridDemoComponent } from './app/ag-grid-demo/ag-grid-demo.component';
   standalone: true,
   providers: [],
   imports: [
+    CommonModule,
     MatDividerModule,
     ReactiveFormsModule,
     MultiSelectComponent,
@@ -31,6 +33,7 @@ export class App {
   name = 'Angular';
   testText = 'test';
   isToggled = true;
+  showContentsArray = [true, false];
 
   handleToggleChange(isToggled: boolean) {
     console.log("testText: ", this.testText);
